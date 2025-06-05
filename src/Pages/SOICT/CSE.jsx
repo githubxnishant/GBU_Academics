7e322903bea72a310b1813cc8dff9944c9b2111f
Nishant Chauhan <mailxnishant@gmail.com>
2025-06-05 09:13:37 +0530

    loader added

--- src/Pages/SOICT/CSE.jsx
+++ src/Pages/SOICT/CSE.jsx
@@ -11,6 +11,7 @@ const Cse = () => {
     const [unsortedData, setUnsortedData] = useState([]);
     const [subData, setSubData] = useState([]);
     const [query, setQuery] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const searchQuery = async () => {
         const subCode = prompt("Search Subject Code");
@@ -31,8 +32,10 @@ const Cse = () => {
     useEffect(() => {
         const fetchAllSubjects = async () => {
             try {
+                setLoading(true);
                 const response = await axios.get(`${server}/viewsubjects/cse`);
                 setUnsortedData(response.data);
+                setLoading(false);
             } catch (error) {
                 console.log("Error Fetching Subject Data from DB!", error);
             }
@@ -65,52 +68,81 @@ const Cse = () => {
                             </div>
                         </div>
                         <div className='flex md:justify-between justify-center flex-wrap gap-5 md:mb-0 -mb-1 h-[31vh] md:w-auto'>
-                            {subData.map((subject) => (
-                                <div className='border-2 bg-white border-[#d9d9d9] h-60 md:w-[30%] w-[80%] rounded p-3 shadow-md'>
-                                    <div className='w-full h-16 bg-[#d9d9d9] rounded flex items-center justify-center text-center mb-2 p-3 text-sm'>{subject.subCode.toUpperCase()} - {subject.subName}</div>
-                                    <div className='h-24 overflow-auto'>
-                                        {subject.year2025.midSem === ' ' && subject.year2025.endSem === ' ' ? '' : <div className='border-2 my-1 h-11 rounded px-7 flex items-center justify-between text-sm'>
-                                            <h1>{subject.year2025.year}</h1>
-                                            <div className='flex items-center justify-center'>
-                                                {subject.year2025.midSem === ' ' ? '' : <a href={subject.year2025.midSem} target='_blank' className='text-[#2563eb]'>Mid</a>}
-                                                {subject.year2025.midSem != ' ' && subject.year2025.endSem != ' ' ? <p className='px-3'>|</p> : ''}
-                                                {subject.year2025.endSem === ' ' ? '' : <a href={subject.year2025.endSem} target='_blank' className='text-[#2563eb]'>End</a>}
+                            {loading ?
+                                <>
+                                    <Loader />
+                                    <Loader />
+                                    <Loader />
+                                    <Loader />
+                                    <Loader />
+                                    <Loader />
+                                </>
+                                :
+                                <>
+                                    {subData.map((subject) => (
+                                        <div className='border-2 bg-white border-[#d9d9d9] h-60 md:w-[30%] w-[80%] rounded p-3 shadow-md'>
+                                            <div className='w-full h-16 bg-[#d9d9d9] rounded flex items-center justify-center text-center mb-2 p-3 text-sm'>{subject.subCode.toUpperCase()} - {subject.subName}</div>
+                                            <div className='h-24 overflow-auto'>
+                                                {subject.year2025.midSem === ' ' && subject.year2025.endSem === ' ' ? '' : <div className='border-2 my-1 h-11 rounded px-7 flex items-center justify-between text-sm'>
+                                                    <h1>{subject.year2025.year}</h1>
+                                                    <div className='flex items-center justify-center'>
+                                                        {subject.year2025.midSem === ' ' ? '' : <a href={subject.year2025.midSem} target='_blank' className='text-[#2563eb]'>Mid</a>}
+                                                        {subject.year2025.midSem != ' ' && subject.year2025.endSem != ' ' ? <p className='px-3'>|</p> : ''}
+                                                        {subject.year2025.endSem === ' ' ? '' : <a href={subject.year2025.endSem} target='_blank' className='text-[#2563eb]'>End</a>}
+                                                    </div>
+                                                </div>}
+                                                {subject.year2024.midSem === ' ' && subject.year2024.endSem === ' ' ? '' : <div className='border-2 my-1 h-11 rounded px-7 flex items-center justify-between text-sm'>
+                                                    <h1>{subject.year2024.year}</h1>
+                                                    <div className='flex items-center justify-center'>
+                                                        {subject.year2024.midSem === ' ' ? '' : <a href={subject.year2024.midSem} target='_blank' className='text-[#2563eb]'>Mid</a>}
+                                                        {subject.year2024.midSem != ' ' && subject.year2024.endSem != ' ' ? <p className='px-3'>|</p> : ''}
+                                                        {subject.year2024.endSem === ' ' ? '' : <a href={subject.year2024.endSem} target='_blank' className='text-[#2563eb]'>End</a>}
+                                                    </div>
+                                                </div>}
+                                                {subject.year2023.midSem === ' ' && subject.year2023.endSem === ' ' ? '' : <div className='border-2 my-1 h-11 rounded px-7 flex items-center justify-between text-sm'>
+                                                    <h1>{subject.year2023.year}</h1>
+                                                    <div className='flex items-center justify-center'>
+                                                        {subject.year2023.midSem === ' ' ? '' : <a href={subject.year2023.midSem} target='_blank' className='text-[#2563eb]'>Mid</a>}
+                                                        {subject.year2023.midSem != ' ' && subject.year2023.endSem != ' ' ? <p className='px-3'>|</p> : ''}
+                                                        {subject.year2023.endSem === ' ' ? '' : <a href={subject.year2023.endSem} target='_blank' className='text-[#2563eb]'>End</a>}
+                                                    </div>
+                                                </div>}
+                                                {subject.year2022.midSem === ' ' && subject.year2022.endSem === ' ' ? '' : <div className='border-2 my-1 h-11 rounded px-7 flex items-center justify-between text-sm'>
+                                                    <h1>{subject.year2022.year}</h1>
+                                                    <div className='flex items-center justify-center'>
+                                                        {subject.year2022.midSem === ' ' ? '' : <a href={subject.year2022.midSem} target='_blank' className='text-[#2563eb]'>Mid</a>}
+                                                        {subject.year2022.midSem != ' ' && subject.year2022.endSem != ' ' ? <p className='px-3'>|</p> : ''}
+                                                        {subject.year2022.endSem === ' ' ? '' : <a href={subject.year2022.endSem} target='_blank' className='text-[#2563eb]'>End</a>}
+                                                    </div>
+                                                </div>}
+                                                {subject.year2025.midSem === ' ' && subject.year2025.endSem === ' ' && subject.year2024.midSem === ' ' && subject.year2024.endSem === ' ' && subject.year2023.midSem === ' ' && subject.year2023.endSem === ' ' && subject.year2022.midSem === ' ' && subject.year2022.endSem === ' ' ? <div className='flex justify-center items-center h-24'>No data available!</div> : ''}
                                             </div>
-                                        </div>}
-                                        {subject.year2024.midSem === ' ' && subject.year2024.endSem === ' ' ? '' : <div className='border-2 my-1 h-11 rounded px-7 flex items-center justify-between text-sm'>
-                                            <h1>{subject.year2024.year}</h1>
-                                            <div className='flex items-center justify-center'>
-                                                {subject.year2024.midSem === ' ' ? '' : <a href={subject.year2024.midSem} target='_blank' className='text-[#2563eb]'>Mid</a>}
-                                                {subject.year2024.midSem != ' ' && subject.year2024.endSem != ' ' ? <p className='px-3'>|</p> : ''}
-                                                {subject.year2024.endSem === ' ' ? '' : <a href={subject.year2024.endSem} target='_blank' className='text-[#2563eb]'>End</a>}
+                                            <div className='h-10 mt-2 bg-[#d9d9d9] rounded flex justify-center items-center'>
+                                                <button className='text-sm hover:font-semibold duration-300 transition-all hover:text-[#2563eb]'>Download Syllabus</button>
                                             </div>
-                                        </div>}
-                                        {subject.year2023.midSem === ' ' && subject.year2023.endSem === ' ' ? '' : <div className='border-2 my-1 h-11 rounded px-7 flex items-center justify-between text-sm'>
-                                            <h1>{subject.year2023.year}</h1>
-                                            <div className='flex items-center justify-center'>
-                                                {subject.year2023.midSem === ' ' ? '' : <a href={subject.year2023.midSem} target='_blank' className='text-[#2563eb]'>Mid</a>}
-                                                {subject.year2023.midSem != ' ' && subject.year2023.endSem != ' ' ? <p className='px-3'>|</p> : ''}
-                                                {subject.year2023.endSem === ' ' ? '' : <a href={subject.year2023.endSem} target='_blank' className='text-[#2563eb]'>End</a>}
-                                            </div>
-                                        </div>}
-                                        {subject.year2022.midSem === ' ' && subject.year2022.endSem === ' ' ? '' : <div className='border-2 my-1 h-11 rounded px-7 flex items-center justify-between text-sm'>
-                                            <h1>{subject.year2022.year}</h1>
-                                            <div className='flex items-center justify-center'>
-                                                {subject.year2022.midSem === ' ' ? '' : <a href={subject.year2022.midSem} target='_blank' className='text-[#2563eb]'>Mid</a>}
-                                                {subject.year2022.midSem != ' ' && subject.year2022.endSem != ' ' ? <p className='px-3'>|</p> : ''}
-                                                {subject.year2022.endSem === ' ' ? '' : <a href={subject.year2022.endSem} target='_blank' className='text-[#2563eb]'>End</a>}
-                                            </div>
-                                        </div>}
-                                        {subject.year2025.midSem === ' ' && subject.year2025.endSem === ' ' && subject.year2024.midSem === ' ' && subject.year2024.endSem === ' ' && subject.year2023.midSem === ' ' && subject.year2023.endSem === ' ' && subject.year2022.midSem === ' ' && subject.year2022.endSem === ' ' ? <div className='flex justify-center items-center h-24'>No data available!</div> : ''}
-                                    </div>
-                                    <div className='h-10 mt-2 bg-[#d9d9d9] rounded flex justify-center items-center'>
-                                        <button className='text-sm hover:font-semibold duration-300 transition-all hover:text-[#2563eb]'>Download Syllabus</button>
-                                    </div>
-                                </div>
-                            ))}
+                                        </div>
+                                    ))}
+                                </>
+                            }
                         </div>
                     </div>
                     <Footer />
+                </div>
+            </div>
+        </>
+    )
+}
+
+const Loader = () => {
+    return (
+        <>
+            <div className='border-2 bg-white border-[#d9d9d9] h-60 md:w-[30%] w-[80%] rounded p-3 shadow-md'>
+                <div className='w-full h-16 bg-[#d9d9d9] rounded flex items-center justify-center text-center mb-2 p-3 text-sm'></div>
+                <div className='h-24 flex items-center flex-col justify-center'>
+                    Loading...
+                </div>
+                <div className='h-10 mt-2 bg-[#d9d9d9] rounded flex justify-center items-center'>
+                    <button className='text-sm hover:font-semibold duration-300 transition-all hover:text-[#2563eb]'></button>
                 </div>
             </div>
         </>
