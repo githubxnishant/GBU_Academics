20a17653fba31c80e3cdeb55ab945e01bdc21033
Nishant Chauhan <mailxnishant@gmail.com>
2025-08-11 18:09:55 +0530

    Update TopLinks.jsx

--- src/Components/Header/TopLinks.jsx
+++ src/Components/Header/TopLinks.jsx
@@ -11,7 +11,7 @@ const TopLinks = () => {
                         <li><a className='cursor-pointer' href='https://www.gbu.ac.in/page/notices' target='_blank'>Notices</a></li>|
                         <li><a className='cursor-pointer' href='https://www.gbu.ac.in/page/events' target='_blank'>Events</a></li>|
                         <li><a className='cursor-pointer' href='https://faculty.gbu.ac.in/' target='_blank'>Faculty</a></li>|
-                                {/* <li><a className='cursor-pointer' href='#' target='_blank'>Sitemap</a></li>| */}
+                        <li><a className='cursor-pointer' href='https://www.gbu.ac.in/academics/academic-programmes' target='_blank'>Syllabus</a></li>|
                         <li><a className='cursor-pointer' href='https://mygbu.in/schd/' target='_blank'>Time Table</a></li>
                     </ul>
                 </div>
